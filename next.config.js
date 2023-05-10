9a7f91a85fe240cc0d964264f614c020ffb3d905
Tom Stirling <tomstirling19@gmail.com>
2023-05-10 20:39:45 +0100

    4 - update deploy config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 module.exports = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/ts-react-next-cv' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/ts-react-next-cv' : '',
   trailingSlash: false,
   webpack: (config) => {
     config.resolve.alias['@/src'] = path.join(__dirname, 'src');
